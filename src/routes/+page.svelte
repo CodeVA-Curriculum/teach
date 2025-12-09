814a1955da9379e4596f89d93377c921ce0d350a
octojon <mail@octojon.dev>
2025-12-09 12:13:09 -0500

    added missing content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,65 @@
   <section>
     <div class='grid'>
       <article>
+        <div class='fullwidth-card'>
+          <div class='img-wrap'>
+          <img src="/library.png"/>
+          </div>
+          <div class='text'>
         <h2>Curriculum Library</h2>
+          <p>The CodeVA Curriculum Library is a free, open resource with high-quality CS lessons for teachers to explore and use in their classrooms. Check out the library and download, remix, or modify lessons to your heart's content!</p>
+          <p>All of our free material is licensed under CC-BY-SA-NC, which means its yours to use as you wish as long as it's not for commercial purposes! Just make sure you credit CodeVA for the materials you adapt from us.</p>
+          <a href="https://curriculum.codevirgnia.org/library" role="button">Visit the Library</a>
+          </div>
+        </div>
       </article>
     </div>
     <div class='grid'>
       <article>
-        <h2>Hub</h2>
+        <h2>CS Education Hub</h2>
+        <p>Your resource for computer science teaching, containing information about CS standards of learning, vocabulary, lesson ideas, and tools to help you design high quality computing instruction for your students.</p> 
+        <button disabled>Coming soon!</button>
       </article>
       <article>
-        <h2>Tools</h2>
+        <h2>CS Integration Tools</h2>
+        <p>Our CS integration and curriculum planning tools provide a starting point for teachers working to integrate computing into their instruction, especially in grades K-8. Check them out!</p>
+          <a href="/tools" role="button">Open the Toolbox</a>
       </article>
     </div>
   </section>
 </div>
+<style lang='scss'>
+  @use "$lib/styles/theme.scss";
+  .fullwidth-card {
+    display: flex;
+    flex-direction: row;
+    .text {
+      justify-content: start;
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+    }
+    img {
+      padding-left: 1rem;
+      padding-right: 1rem;
+      flex-grow: 0;
+    }
+  }
+  article {
+    display: flex;
+    flex-direction: column;
+  }
+  button, a {
+    align-items:flex-end;  
+    width: 100%;
+    margin-top: auto;
+    margin-bottom: 0;
+    background-color: theme.$navy;
+  }
+  p {
+    flex-grow: 1;
+    a {
+      background-color: white;
+    }
+  }
+</style>
